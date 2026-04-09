9134d0a6ada0ff7d23e4e6b2cf50f357e5f4da6f
bankeyaditya <bankeyaditya7@gmail.com>
2026-04-09 23:13:16 +0530

    changing a next.config.ts to .js file

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ warnings won't block Vercel builds
-  },
-};
-
-export default nextConfig;
